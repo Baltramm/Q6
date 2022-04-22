0f44f7dbe4390098d49e6d5406d0a98d3350cfeb
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-22 22:32:50 +0600

    Задание 6.5.2

--- Q6/Program.cs
+++ Q6/Program.cs
@@ -10,22 +10,45 @@ namespace Q6
 
         }
     }
-        class Bus
+    class Triangle
+    {
+        public int a;
+        public int b;
+        public int c;
+
+        public int P()
         {
-            public int? Load;
-        
-            public void PrintStatus()
-            {
-          
-            if (Load.HasValue)
-            {
-                Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
-            }
-            else
-            {
-                Console.WriteLine("Автобус пуст");
-            }
-            }
+            return a + c + b;
         }
+        public double S()
+        {
+            int p = P();
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+    }
+    class Circle
+    {
+        public int r;
+        public double S()
+        {
+            return r*r*Math.PI;
+        }
+        public double P()
+        {
+            return 2*Math.PI*r;
+        }
+    }
+    class Square
+    {
+        public int a;
+        public int P()
+        {
+            return a*4;
+        }
+        public double S()
+        {
+            return a*a;
+        }
+    }
 
     }
